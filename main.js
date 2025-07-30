84e4ae007e03dcf00bea50d2a57b5cac9bc0d791
ETYoumans <ethanyoumans@outlook.com>
2025-07-30 00:47:25 -0400

    Electron Fixes

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const { app, BrowserWindow } = require('electron');
-
+const path = require('path');
 app.whenReady().then(() => {
-  const win = new BrowserWindow({ width: 800, height: 600 });
-  win.loadURL('index.html');
+  const win = new BrowserWindow({ width: 1920, height: 1080 });
+  win.loadFile(path.join(__dirname, 'index.html'));
 });
